cdd232a61ca9ca2c2d03516014bcf400d3f9ba07
GollyGreg <benjaminbrimeyer@gmail.com>
2019-03-13 02:06:43 -0700

    Updated default example

--- examples/default.client.lua
+++ examples/default.client.lua
@@ -11,11 +11,13 @@ myScreenGui.Name = "ObjectHighlighter"
 myScreenGui.Parent = LocalPlayer.PlayerGui
 
 local myHighlight = ObjectHighlighter.Highlight.fromTarget(workspace.Model)
+myHighlight.color = Color3.fromRGB(255, 255, 0)
+
 local myRenderer = ObjectHighlighter.Renderer.new(myScreenGui)
-	:withRenderImpl(ObjectHighlighter.Renderer.Implementations.tagged)
+	:withRenderImpl(ObjectHighlighter.Implementations.highlightColor)
 
 myRenderer:addToStack(myHighlight)
 
-RunService.Heartbeat:Connect(function(dt)
+RunService.RenderStepped:Connect(function(dt)
 	myRenderer:step(dt)
 end)
