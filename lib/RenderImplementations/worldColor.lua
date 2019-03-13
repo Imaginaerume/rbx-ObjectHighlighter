66d37ecdd60c76fc691faa8fdc8db84ae6c13c45
GollyGreg <benjaminbrimeyer@gmail.com>
2019-03-13 02:01:22 -0700

    Default highlightColor and worldColor render implementations added

---
+++ lib/RenderImplementations/worldColor.lua
@@ -0,0 +1,24 @@
+return function()
+	local connections = {}
+
+	return {
+		onRender = function(_, worldPart, viewportPart, _)
+			viewportPart.CFrame = worldPart.CFrame
+		end,
+
+		onAdded = function(worldPart, viewportPart, _)
+			viewportPart.Color = worldPart.Color
+
+			connections[worldPart] = worldPart:GetPropertyChangedSignal("Color"):Connect(function()
+				viewportPart.Color = worldPart.Color
+			end)
+		end,
+
+		onRemoved = function(worldPart, _, _)
+			if connections[worldPart] then
+				connections[worldPart]:Disconnect()
+				connections[worldPart] = nil
+			end
+		end,
+	}
+end
